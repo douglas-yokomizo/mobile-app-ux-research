3d29832cd50349da5ed132dbdd2d0d4ec14d73d0
Douglas Yokomizo <yogi.yokomizo@gmail.com>
2024-07-22 18:38:05 -0300

    feat: add all the sections in home page

---
+++ app/assets/homePage/index.js
@@ -0,0 +1,51 @@
+import alertIcon from "./alert.svg";
+import handHeart from "./Hand-heart.svg";
+import globe from "./globe.svg";
+import guardian from "./guardian.svg";
+import coin from "./coin.svg";
+import phoneStatus from "./phone-status.svg";
+import shield from "./shield.svg";
+import starterShield from "./starter.svg";
+import gold from "./gold.svg";
+import weekGoal from "./weekGoal.svg";
+import progressBar from "./progressBar.svg";
+import avatar from "../avatar.png";
+import arrow from "../menu/arrow.svg";
+import fullyLogo from "./fullyLogo.png";
+import medal from "./medal.svg";
+import steps from "./steps.svg";
+import heart from "./heart.svg";
+import time from "./time.svg";
+import goodOrange from "./goodOrange.svg";
+import goodGreen from "./goodGreen.svg";
+import regular from "./regular.svg";
+import bannerHome from "./bannerHome.png";
+import phone from "./phone.svg";
+import whatsapp from "./whatsapp.svg";
+
+export {
+  alertIcon,
+  handHeart,
+  globe,
+  guardian,
+  coin,
+  phoneStatus,
+  shield,
+  starterShield,
+  gold,
+  weekGoal,
+  progressBar,
+  avatar,
+  arrow,
+  fullyLogo,
+  medal,
+  steps,
+  heart,
+  time,
+  goodOrange,
+  goodGreen,
+  regular,
+  bannerHome,
+  phone,
+  whatsapp,
+};
