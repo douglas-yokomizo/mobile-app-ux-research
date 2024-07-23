85d138c04581891203a3ddc00eb98bfdb738158b
Rafael Feitosa <rafaefeitosa@gmail.com>
2024-07-23 15:33:02 -0300

    refactor: folder structure

--- app/data/extrato.js
+++ app/data/extrato.js
@@ -1,16 +1,16 @@
 const extrato = [
-  { mes: "Dezembro", total: "R$1200,45", status: "Pago" },
-  { mes: "Novembro", total: "R$1200,45", status: "Pago" },
-  { mes: "Outubro", total: "R$1200,45", status: "Pago" },
-  { mes: "Setembro", total: "R$1200,45", status: "Pago" },
-  { mes: "Agosto", total: "R$1200,45", status: "Pago" },
-  { mes: "Julho", total: "R$1200,45", status: "Pago" },
-  { mes: "Junho", total: "R$1200,45", status: "Pago" },
-  { mes: "Maio", total: "R$1200,45", status: "Pago" },
-  { mes: "Abril", total: "R$1200,45", status: "Pago" },
-  { mes: "Março", total: "R$1200,45", status: "Pago" },
-  { mes: "Fevereiro", total: "R$1200,45", status: "Pago" },
-  { mes: "Janeiro", total: "R$1200,45", status: "Pago" },
+	{ mes: "Dezembro", total: "R$1200,45", status: "Pago" },
+	{ mes: "Novembro", total: "R$1200,45", status: "Pago" },
+	{ mes: "Outubro", total: "R$1200,45", status: "Pago" },
+	{ mes: "Setembro", total: "R$1200,45", status: "Pago" },
+	{ mes: "Agosto", total: "R$1200,45", status: "Pago" },
+	{ mes: "Julho", total: "R$1200,45", status: "Pago" },
+	{ mes: "Junho", total: "R$1200,45", status: "Pago" },
+	{ mes: "Maio", total: "R$1200,45", status: "Pago" },
+	{ mes: "Abril", total: "R$1200,45", status: "Pago" },
+	{ mes: "Março", total: "R$1200,45", status: "Pago" },
+	{ mes: "Fevereiro", total: "R$1200,45", status: "Pago" },
+	{ mes: "Janeiro", total: "R$1200,45", status: "Pago" },
 ];
 
 export default extrato;
--- app/data/extratoDetails.js
+++ app/data/extratoDetails.js
@@ -1,51 +1,49 @@
-
 const dados = [
-    {
-        status: "Pago",
-        nome: "Carlos Henrique Ferreira de Souza",
-        data: "08/01/24",
-        apolice: "Vida Inteira",
-        valor: "R$ 400,45",
-        method: "Mastercard",
-        methodDetails: "**** ***** **** 8842",
-      },
-      {
-        status: "Pago",
-        nome: "Pedro Ferreira de Souza",
-        data: "01/01/24",
-        apolice: "Filhos",
-        valor: "R$ 600,00",
-        method: "Mastercard",
-        methodDetails: "**** ***** **** 8842",
-    },
-    {
-      status: "Pago",
-      nome: "Ana Lucia Ferreira de Souza",
-      data: "01/01/24",
-      apolice: "Esposa",
-      valor: "R$ 200,00",
-      method: "Banco Itaú",
-      methodDetails: "Ag: 0000 / Conta: 00000-0",
-    },
-    {
-        status: "Pago",
-        nome: "Maria Ferreira de Souza",
-        data: "01/01/24",
-        apolice: "Filhos",
-        valor: "R$ 600,00",
-        method: "Mastercard",
-        methodDetails: "**** ***** **** 8842",
-    },
-    {
-        status: "Pago",
-        nome: "João Ferreira de Souza",
-        data: "01/01/24",
-        apolice: "Filhos",
-        valor: "R$ 600,00",
-        method: "Mastercard",
-        methodDetails: "**** ***** **** 8842",
-    },
-  ];
-  
-  export default dados;
-  
+	{
+		status: "Pago",
+		nome: "Carlos Henrique Ferreira de Souza",
+		data: "08/01/24",
+		apolice: "Vida Inteira",
+		valor: "R$ 400,45",
+		method: "Mastercard",
+		methodDetails: "**** ***** **** 8842",
+	},
+	{
+		status: "Pago",
+		nome: "Pedro Ferreira de Souza",
+		data: "01/01/24",
+		apolice: "Filhos",
+		valor: "R$ 600,00",
+		method: "Mastercard",
+		methodDetails: "**** ***** **** 8842",
+	},
+	{
+		status: "Pago",
+		nome: "Ana Lucia Ferreira de Souza",
+		data: "01/01/24",
+		apolice: "Esposa",
+		valor: "R$ 200,00",
+		method: "Banco Itaú",
+		methodDetails: "Ag: 0000 / Conta: 00000-0",
+	},
+	{
+		status: "Pago",
+		nome: "Maria Ferreira de Souza",
+		data: "01/01/24",
+		apolice: "Filhos",
+		valor: "R$ 600,00",
+		method: "Mastercard",
+		methodDetails: "**** ***** **** 8842",
+	},
+	{
+		status: "Pago",
+		nome: "João Ferreira de Souza",
+		data: "01/01/24",
+		apolice: "Filhos",
+		valor: "R$ 600,00",
+		method: "Mastercard",
+		methodDetails: "**** ***** **** 8842",
+	},
+];
+
+export default dados;
