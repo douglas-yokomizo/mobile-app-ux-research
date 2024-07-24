c898c6c523005cee2e9055fbc964036920a3b9a2
Douglas Yokomizo <yogi.yokomizo@gmail.com>
2024-07-24 08:23:38 -0300

    feat: add Brokers page  and brokers data

---
+++ app/brokers/page.jsx
@@ -0,0 +1,5 @@
+const BrokersPage = () => {
+  return <div>page</div>;
+};
+
+export default BrokersPage;
---
+++ app/data/brokers.js
@@ -0,0 +1,44 @@
+const brokers = [
+  {
+    name: "Túlio dos Santos",
+    company: "AC Corretores de Saúde LTDA",
+    cnpj: "231.266.97/0001-47",
+    email: "tulio.santos@gmail.com",
+    phone: "+55 (21) 99944-9876",
+    policies: [
+      {
+        name: "Colégio filhos",
+        number: "001501123",
+      },
+      {
+        name: "Família",
+        number: "001528345",
+      },
+      {
+        name: "Doenças graves",
+        number: "001542212",
+      },
+      {
+        name: "Doenças graves",
+        number: "001543914",
+      },
+    ],
+  },
+  {
+    name: "Daniella França",
+    company: " BET corretores de saúde LTDA",
+    cnpj: "231.266.97/0001-47",
+    email: "daniella.franca@gmail.com",
+    phone: "+55 (21) 99944-9876",
+    policies: [
+      {
+        name: "Proteção em vida",
+        number: "001501123",
+      },
+      {
+        name: "Família",
+        number: "001528345",
+      },
+    ],
+  },
+];
