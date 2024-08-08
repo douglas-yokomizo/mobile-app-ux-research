00884c539d1fc157d1fa930cdc06646ab120bd81
PedroTresmondi <pedro_tresmondi@hotmail.com>
2024-08-08 16:21:31 -0300

    Adding timer to remove inactive sessions

--- app/(auth)/auth/page.js
+++ app/(auth)/auth/page.js
@@ -1,7 +1,6 @@
 "use client";
 
-import { useState, useEffect, useRef } from "react";
-import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
 import { supabase } from "../../lib/supabaseClient";
 import { useGame } from "@/app/hooks/useGame";
 import { CHALLENGES } from "../../data/challenges";
@@ -11,205 +10,232 @@ import PrudentialLogo from "./Prudential-Logo.png";
 import Image from "next/image";
 import { RunnerIcon } from "./RunnerIcon";
 
-//deus esta morto
 export default function Auth() {
-	const [message, setMessage] = useState("");
-	const { setGameSession, setGamePlayer, session } = useGame();
-	const [countdown, setCountdown] = useState(5);
-	const [gameStarted, setGameStarted] = useState(false);
-	const [waiting, setWaiting] = useState(false);
+    const [message, setMessage] = useState("");
+    const { setGameSession, setGamePlayer, session } = useGame();
+    const [countdown, setCountdown] = useState(5);
+    const [gameStarted, setGameStarted] = useState(false);
+    const [waiting, setWaiting] = useState(false);
 
-	useEffect(() => {
-		const subscription = supabase
-			.channel("realtime:sessions")
-			.on(
-				"postgres_changes",
-				{ event: "*", schema: "public", table: "sessions" },
-				async (payload) => {
-					console.log("Payload received:", payload);
-					const newSession = payload.new;
-					if (newSession.is_active) {
-						if (newSession.game_started) {
-							setGameStarted(true);
-						}
-					}
-				},
-			)
-			.subscribe();
+    useEffect(() => {
+        const subscription = supabase
+            .channel("realtime:sessions")
+            .on(
+                "postgres_changes",
+                { event: "*", schema: "public", table: "sessions" },
+                async (payload) => {
+                    const newSession = payload.new;
+                    if (newSession.is_active) {
+                        setGameSession(newSession);
+                        if (newSession.game_started) {
+                            setGameStarted(true);
+                        }
+                    }
+                }
+            )
+            .subscribe();
 
-		return () => {
-			supabase.removeChannel(subscription);
-		};
-	}, []);
+        return () => {
+            supabase.removeChannel(subscription);
+        };
+    }, []);
 
-	useEffect(() => {
-		if (waiting) {
-			let index = 0;
-			const messages = [
-				"Sessão criada, aguardando outro jogador.",
-				"Sessão criada, aguardando outro jogador..",
-				"Sessão criada, aguardando outro jogador...",
-			];
-			const interval = setInterval(() => {
-				setMessage(messages[index]);
-				index = (index + 1) % messages.length;
-			}, 1000);
+    useEffect(() => {
+        if (waiting) {
+            let index = 0;
+            const messages = [
+                "Sessão criada, aguardando outro jogador.",
+                "Sessão criada, aguardando outro jogador..",
+                "Sessão criada, aguardando outro jogador...",
+            ];
+            const interval = setInterval(() => {
+                setMessage(messages[index]);
+                index = (index + 1) % messages.length;
+            }, 1000);
 
-			return () => clearInterval(interval);
-		}
-	}, [waiting]);
+            const timeout = setTimeout(async () => {
+                const { data: sessionData, error: sessionError } = await supabase
+                    .from("sessions")
+                    .select("*")
+                    .eq("id", session.id)
+                    .single();
 
-	const startGame = async () => {
-		try {
-			// Criar um novo jogador com nome aleatório
-			const playerName = `Player_${Math.random().toString(36).substring(2, 15)}`;
-			const { data: user, error: userError } = await supabase
-				.from("users")
-				.insert([{ name: playerName }])
-				.select()
-				.single();
+                if (sessionError) {
+                    console.error("Erro ao verificar sessão:", sessionError);
+                    return;
+                }
 
-			if (userError) {
-				console.error("Erro ao criar jogador:", userError);
-				return;
-			}
+                if (!sessionData.player2_id) {
+                    const { error: deleteError } = await supabase
+                        .from("sessions")
+                        .delete()
+                        .eq("id", session.id);
 
-			if (!user) {
-				return;
-			}
+                    if (deleteError) {
+                        console.error("Erro ao deletar sessão:", deleteError);
+                    } else {
+                        setWaiting(false);
+                        setGameSession(null);
+                        console.log("Sessão deletada devido à ausência do jogador 2.");
+                    }
+                }
+            }, 3 * 60 * 1000);
 
-			setGamePlayer(user);
-			const { data: sessions, error: sessionsError } = await supabase
-				.from("sessions")
-				.select("*")
-				.eq("is_active", false)
-				.is("player2_id", null);
+            return () => {
+                clearInterval(interval);
+                clearTimeout(timeout);
+            };
+        }
+    }, [waiting]);
 
-			if (sessionsError) {
-				console.error("Erro ao buscar sessões:", sessionsError);
-				return;
-			}
+    const startGame = async () => {
+        try {
+            const playerName = `Player_${Math.random().toString(36).substring(2, 15)}`;
+            const { data: user, error: userError } = await supabase
+                .from("users")
+                .insert([{ name: playerName }])
+                .select()
+                .single();
 
-			let session;
-			if (sessions.length === 0) {
-				const { data: newSession, error: newSessionError } = await supabase
-					.from("sessions")
-					.insert([{ player1_id: user.id, is_active: false }])
-					.select()
-					.single();
+            if (userError) {
+                console.error("Erro ao criar jogador:", userError);
+                return;
+            }
 
-				if (newSessionError) {
-					console.error("Erro ao criar a sessão:", newSessionError);
-					return;
-				}
+            if (!user) {
+                return;
+            }
 
-				session = newSession;
-				setWaiting(true); // Inicia a animação
-			} else {
-				session = sessions[0];
-				const challenge = CHALLENGES[~~(Math.random() * CHALLENGES.length)];
+            setGamePlayer(user);
+            const { data: sessions, error: sessionsError } = await supabase
+                .from("sessions")
+                .select("*")
+                .eq("is_active", false)
+                .is("player2_id", null);
 
-				const { data: updatedSession, error: updateSessionError } =
-					await supabase
-						.from("sessions")
-						.update({
-							player2_id: user.id,
-							is_active: true,
-							game_challenge: challenge,
-							game_started: sessions.length > 0,
-							countdown_start_time:
-								sessions.length > 0 ? new Date().toISOString() : null,
-						})
-						.eq("id", session.id)
-						.select()
-						.single();
+            if (sessionsError) {
+                console.error("Erro ao buscar sessões:", sessionsError);
+                return;
+            }
 
-				if (updateSessionError) {
-					console.error("Erro ao entrar na sessão:", updateSessionError);
-					return;
-				}
+            let session;
+            if (sessions.length === 0) {
+                const { data: newSession, error: newSessionError } = await supabase
+                    .from("sessions")
+                    .insert([{ player1_id: user.id, is_active: false }])
+                    .select()
+                    .single();
 
-				session = updatedSession;
-			}
+                if (newSessionError) {
+                    console.error("Erro ao criar a sessão:", newSessionError);
+                    return;
+                }
 
-			setGameSession(session);
-		} catch (err) {
-			console.error("Erro inesperado:", err);
-		}
-	};
+                session = newSession;
+                setWaiting(true);
+            } else {
+                session = sessions[0];
+                const challenge = CHALLENGES[~~(Math.random() * CHALLENGES.length)];
 
-	useEffect(() => {
-		if (gameStarted) {
-			const timer =
-				countdown > 0 && setInterval(() => setCountdown(countdown - 1), 1000);
-			if (countdown === 0) {
-				clearInterval(timer);
-				return window.location.replace("/home");
-			}
+                const { data: updatedSession, error: updateSessionError } =
+                    await supabase
+                        .from("sessions")
+                        .update({
+                            player2_id: user.id,
+                            is_active: true,
+                            game_challenge: challenge,
+                            game_started: sessions.length > 0,
+                            countdown_start_time:
+                                sessions.length > 0 ? new Date().toISOString() : null,
+                        })
+                        .eq("id", session.id)
+                        .select()
+                        .single();
 
-			return () => clearInterval(timer);
-		}
-	}, [gameStarted, countdown]);
+                if (updateSessionError) {
+                    console.error("Erro ao entrar na sessão:", updateSessionError);
+                    return;
+                }
 
-	return (
-		<div className="flex items-center justify-center min-h-screen bg-gradient-to-b from-[#1F508E] to-[#031F45] text-white flex-col">
-			<Image src={PrudentialLogo} className=" my-10 fixed top-0 w-[300px]" />
+                session = updatedSession;
+            }
 
-			<div className="w-full  max-w-2xl p-9 space-y-10 bg-[#FCFCFC] rounded-xl shadow-lg text-[#1F1F24]">
-				{gameStarted ? (
-					<>
-						<span className="w-full items-center justify-center flex">
-							<RunnerIcon />
-						</span>
-						<p className="text-center text-2xl">Seu objetivo é</p>
+            setGameSession(session);
+        } catch (err) {
+            console.error("Erro inesperado:", err);
+        }
+    };
 
-						<p className=" font-bold text-3xl text-center">
-							{session.game_challenge[0].toUpperCase() +
-								session.game_challenge.substring(1).toLowerCase()}
-						</p>
+    useEffect(() => {
+        if (gameStarted) {
+            const timer =
+                countdown > 0 && setInterval(() => setCountdown(countdown - 1), 1000);
+            if (countdown === 0) {
+                clearInterval(timer);
+                return window.location.replace("/home");
+            }
 
-						<div className="flex items-center flex-col gap-2 p-6 bg-[#F0EFF2] rounded-3xl">
-							<p className="text-xl">O desafio vai começar em...</p>
+            return () => clearInterval(timer);
+        }
+    }, [gameStarted, countdown]);
 
-							<p className="font-bold text-3xl">{countdown}</p>
-						</div>
-					</>
-				) : (
-					<>
-						<span className="w-full items-center justify-center flex">
-							<StartIcon />
-						</span>
-						<h2 className="font-bold text-center text-6xl text-black">
-							Boas vindas
-						</h2>
-						<p className="text-center text-2xl ">
-							O aplicativo Prudential vem aí, por isso <br />
-							preparamos esse jogo para a rede de
-							<br /> franquia conhecer algumas
-							<br /> funcionalidades do app.
-						</p>
-						<p className="text-center text-2xl">
-							Para vencer, seja ágil e complete seu
-							<br /> objetivo antes do seu adversário.
-						</p>
-						<button
-							onClick={startGame}
-							className="w-full p-6 text-xl text-white bg-blue-600 rounded-full hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-500 disabled:bg-[#F0EFF2] disabled:text-[#5E5E63]"
-							disabled={waiting}
-							type="button"
-						>
-							{waiting ? (
-								<div className="flex items-center justify-center gap-6">
-									<Spinner height="w-7" />
-									Aguardando adversário
-								</div>
-							) : (
-								"Começar a jogar"
-							)}
-						</button>
-					</>
-				)}
-			</div>
-		</div>
-	);
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gradient-to-b from-[#1F508E] to-[#031F45] text-white flex-col">
+            <Image src={PrudentialLogo} className=" my-10 fixed top-0 w-[300px]" />
+
+            <div className="w-full max-w-2xl p-9 space-y-10 bg-[#FCFCFC] rounded-xl shadow-lg text-[#1F1F24]">
+                {gameStarted ? (
+                    <>
+                        <span className="w-full items-center justify-center flex">
+                            <RunnerIcon />
+                        </span>
+                        <p className="text-center text-2xl">Seu objetivo é</p>
+
+                        <p className="font-bold text-3xl text-center">
+                            {session.game_challenge[0].toUpperCase() +
+                                session.game_challenge.substring(1).toLowerCase()}
+                        </p>
+
+                        <div className="flex items-center flex-col gap-2 p-6 bg-[#F0EFF2] rounded-3xl">
+                            <p className="text-xl">O desafio vai começar em...</p>
+
+                            <p className="font-bold text-3xl">{countdown}</p>
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        <span className="w-full items-center justify-center flex">
+                            <StartIcon />
+                        </span>
+                        <h2 className="font-bold text-center text-6xl text-black">
+                            Boas vindas
+                        </h2>
+                        <p className="text-center text-2xl ">
+                            O aplicativo Prudential vem aí, por isso <br />
+                            preparamos esse jogo para a rede de franquia conhecer algumas
+                            <br /> funcionalidades do app.
+                        </p>
+                        <p className="text-center text-2xl">
+                            Para vencer, seja ágil e complete seu objetivo antes do seu adversário.
+                        </p>
+                        <button
+                            onClick={startGame}
+                            className="w-full p-6 text-xl text-white bg-blue-600 rounded-full hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-500 disabled:bg-[#F0EFF2] disabled:text-[#5E5E63]"
+                            disabled={waiting}
+                            type="button"
+                        >
+                            {waiting ? (
+                                <div className="flex items-center justify-center gap-6">
+                                    <Spinner height="w-7" />
+                                    Aguardando adversário
+                                </div>
+                            ) : (
+                                "Começar a jogar"
+                            )}
+                        </button>
+                    </>
+                )}
+            </div>
+        </div>
+    );
 }
